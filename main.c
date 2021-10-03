797188e4ffb0c30b3005303d893014c8286639dc
Zekeriya AY <z3keriyaay@gmail.com>
2021-10-03 15:14:11 +0300

    ✨ Made Changes.

--- main.c
+++ main.c
@@ -4,27 +4,80 @@
 #include <string.h>
 
 const char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+const char targetPasswd[] = "99VEC49CQ4cf";
 
-int randNum(int n) {
-    return rand() % n;
-}
-
-char *randomString(int len) {
+char *generateRandomString(int len) {
     char *output = malloc(len * sizeof(char));
 
     for (int i = 0; i < len; i++) {
-        output[i] = alphabet[randNum(strlen(alphabet))];
+        output[i] = alphabet[rand() % strlen(alphabet)];
     }
 
     return output;
 }
 
+char *generateSortedString(int len, int count) {
+    char *output = malloc(len * sizeof(char));
+
+    for (int i = 0; i < len; i++) {
+        for(int j = count; j < strlen(alphabet); j++) {
+            output[i] = alphabet[j];
+            break;
+        }
+    }
+
+    return output;
+}
+
+int printOutput(int count, char *beFoundPasswd) {
+    FILE *fptr;
+
+    fptr = fopen("result_passwd.txt","w");
+    if(fptr == NULL){
+        printf("\n[!]FILE ERROR!\n");
+        exit(0);
+    }
+
+    fprintf(fptr,"[+]Target Passwd: %s\t TEST %d --> Be Found Passwd: %s\n",targetPasswd,count,beFoundPasswd);
+    printf("\n[+]Target Passwd: %s\t TEST %d --> Be Found Passwd: %s\n",targetPasswd,count,beFoundPasswd);
+    
+    fclose(fptr);
+    return 1;
+}
+
+int findPasswd(int len){
+    int count = 0;
+    int running = 1;
+    
+    while (running) {
+        /* 
+         * Memory management magic by: Berkay Çubuk <berkaycubuk.com>
+         */
+
+        //char *passwd = generateRandomString(len);
+        char *passwd = generateSortedString(len, count);
+        count++;
+
+        if (!(count % 100000000)) {
+            printf("[-]COUNT %d\n",count);
+        }
+        
+        if (passwd == targetPasswd) {
+            running = 0;
+            printOutput(count, passwd);
+        }
+        free(passwd);
+    }
+
+    return 1;
+}
+
 int main() {
     struct timespec ts;
     clock_gettime(CLOCK_MONOTONIC, &ts);
-
     srand((time_t)ts.tv_nsec);
-    printf("Output: %s\n", randomString(12));
+    
+    findPasswd(12);
 
     return 1;
 }
